39fd279112055c35ac32ad9e1fb6888f5f0d81ed
GIOFATT <85105011+GIOFATT@users.noreply.github.com>
2023-11-24 12:04:14 +0900

    Delete main.py

--- main.py
+++
@@ -1,7 +0,0 @@
-from sito import create_app  # Possibile perche sito ha init.py che lo rende un pacchetto python
-
-app = create_app()
-
-if __name__ == '__main__':
-    app.run(debug=True)
-    # debug permette di rerunnare il programma ad ogni cambiamento del sito
